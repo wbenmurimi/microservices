ef2ed4c9db340f25c558ed27bc55492c3878a399
Benson Murimi <wbenmurimi@gmail.com>
2020-03-30 23:12:36 +0200

    Microservices Demo Applications

--- orders/index.js
+++ orders/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 const mongoose = require('mongoose');
-const axios = require('axios');
 var bodyParser = require('body-parser');
 
 require('dotenv').config();
--- products/index.js
+++ products/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 const mongoose = require('mongoose');
 const axios = require('axios');
 var bodyParser = require('body-parser');
